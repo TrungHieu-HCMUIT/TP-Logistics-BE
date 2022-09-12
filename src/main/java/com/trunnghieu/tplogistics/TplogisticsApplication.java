5c3ed6701dbce94022a3db9387a95654aedb6736
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2022-09-12 17:53:38 +0700

    Init

---
+++ src/main/java/com/trunnghieu/tplogistics/TplogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.trunnghieu.tplogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TplogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TplogisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trunnghieu/tplogistics/TplogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trunnghieu.tplogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TplogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
